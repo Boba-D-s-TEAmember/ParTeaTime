2ba7e5ff8cf2ef2031b2aeb859003e19a67d2a4b
siam132 <shahidullah.rahman223@gmail.com>
2020-05-02 18:08:02 -0400

    Added functionality for post endpoint

--- server/routers/posts.js
+++ server/routers/posts.js
@@ -3,19 +3,23 @@ const router = express.Router();
 const Post = require("../models/post");
 
 router.get("/", async (req, res) => {
-    const posts = await Post.find(); 
-    res.json(posts)
+  const posts = await Post.find();
+  res.json(posts);
 });
 
-router.post("/", async(req, res) => {
+router.post("/", async (req, res) => {
   const post = new Post({
     title: req.body.title,
     caption: req.body.caption,
   });
 
-   const savedPost = await post.save()
-    
-   res.json(savedPost);
+  const savedPost = await post.save();
+  res.json(savedPost);
+});
+
+router.delete("/:id", async (req, res) => {
+  const deletedPost = await Post.deleteOne({ _id: req.params.id });
+  res.json(deletedPost);
 });
 
 module.exports = router;
