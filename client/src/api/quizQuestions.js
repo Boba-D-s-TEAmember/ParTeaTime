e88cf26bd9ed76a3ac161113a90b339c5b95fd31
Cat_exe <carolpignato97@gmail.com>
2020-05-03 02:05:56 -0400

    made quizpage in views

---
+++ client/src/api/quizQuestions.js
@@ -0,0 +1,90 @@
+var quizQuestions = [
+    {
+        question: "Fruit tea or milk tea?",
+        answers: [
+            {
+                type: "Lychee Fruit Tea",
+                content: "Fruit Tea"
+            },
+            {
+                type: "Brown Sugar Milk Tea",
+                content: "Milk Tea"
+            },
+            {
+                type: "Green Tea",
+                content: "Depends on how I feel"
+            }
+        ]
+    },
+    {
+        question: "When do you drink your tea?",
+        answers: [
+            {
+                type: "Lychee Fruit Tea",
+                content: "During friend hangouts."
+            },
+            {
+                type: "Brown Sugar Boba Milk Tea",
+                content: "At least once a week."
+            },
+            {
+                type: "Green Tea",
+                content: "Once in a while."
+            }
+        ]
+    },
+    {
+        question: "At a Chinese restaurant, you see a person request sugar for their hot oolong tea. What are your thoughts on this?",
+        answers: [
+            {
+                type: "Lychee Fruit Tea",
+                content: "People can drink their tea however they want."
+            },
+            {
+                type: "Brown Sugar Boba Milk Tea",
+                content: "OMG I thought I was the only one!"
+            },
+            {
+                type: "Green Tea",
+                content: "Judging them silently - oolong tea should be drinken plain."
+            }
+        ]
+    },
+    {
+        question: "Tea bags or tea leaves?",
+        answers: [
+            {
+                type: "Lychee Fruit Tea",
+                content: "I'm fine with either."
+            },
+            {
+                type: "Brown Sugar Boba Milk Tea",
+                content: "Tea bags are easier!"
+            },
+            {
+                type: "Green Tea",
+                content: "Tea leaves are the best!"
+            }
+        ]
+    },
+    {
+        question: "How do you feel about dairy?",
+        answers: [
+            {
+                type: "Lychee Fruit Tea",
+                content: "I can drink it."
+            },
+            {
+                type: "Brown Sugar Boba Milk Tea",
+                content: "I love it"
+            },
+            {
+                type: "Green Tea",
+                content: "I am lactose intolerant."
+            }
+        ]
+    }
+  ];
+  
+  export default quizQuestions;
+  
