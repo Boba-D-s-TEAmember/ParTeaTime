ccdfe5357f6a70f0a63094613593c49d20fc4677
Rohma-Khan <53379843+Rohma-Khan@users.noreply.github.com>
2020-05-03 03:20:16 -0400

    Added a descriptio and some font changes

--- client/src/views/About.jsx
+++ client/src/views/About.jsx
@@ -2,14 +2,23 @@ import React from 'react'
 import { useState } from "react";
 
 
-
 function About(props) {
-    const [name, setName] = useState("");
   
-    return (
-      <div className="login-view">
-       <h1>This is about page</h1>
+    return ((
+      <div className="AboutUs-view">
+        <h1 className="header3">Welcome!</h1>
+        <h1 className="header4">About:</h1>
+        <h2 className="header4">We are so happy to have you here! We created this as a way to connect different boba or tea enthusiasts together. We will connect people with others who are “drinking” the same boba flavors as them to chat, and share anything related to boba. We will give you the option to upload your own photos, captions and titles. Grab a comforting hot drink and get the coffee shop experience.</h2>
+      <div className="test">
+        <div className="button">
+        <button>Take the Bobba Quiz</button>
+        </div>
+        <div className="button">
+        <button>Go to Our Feed</button>
+        </div>
       </div>
+      </div>
+    )
     );
   }
   
